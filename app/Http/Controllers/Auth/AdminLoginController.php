82b97e301fe28b1e0ce5ac1533799266b3f5dcf1
Christos Dolopikos <cdolopikos.cd@gmail.com>
2019-04-21 12:56:10 +0300

    more

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,14 +4,32 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminLoginController extends Controller
 {
+    public function __construct(){
+      $this->middleware('guest:admin');
+    }
+
     public function showLoginForm(){
       return view('auth.admin-login');
     }
 
     public function login(){
-      return true;
+      //validate the form data
+      $this -> validate($request, [
+        'email' => 'required|email',
+        'password' => 'required|min:6'
+      ]);
+
+      //attempt to log the user data in
+      if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)){
+        //if successdull, then redirect to their intended location
+        return redirect()->intended(route('amdin.dashboard'));
+      }
+
+      return redirect()->back()->withInput($request->only('email', 'remember'));
+      //if unsuccesdfull, rdirect back to the login with the form data
     }
 }
